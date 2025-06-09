498a4fef25b136a699c89c7b0f5ea41e1c4f56a8
Nztorz <nztor@proton.me>
2025-06-09 12:55:09 -0600

    Error Handlers Added in app.js

--- backend/app.js
+++ backend/app.js
@@ -45,11 +45,45 @@ app.use(
 
 // routes
 const routes = require('./routes');
-
+const { ValidationError } = require('sequelize');
 app.use(routes);
 
 
+// error handlers
+// 404 error
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = {message: "The requested resource couldn't be found."};
+    err.status = 404;
+    next(err);
+});
 
+// process sequelize errors
+app.use((err, _req, _res, next) => {
+    // sequelize error?
+    if(err instanceof ValidationError) {
+        let errors = {};
+        for(let error of err.errors) {
+            errors[error.path] = error.message;
+        }
+        err.title = "Validation error";
+        err.errors = errors;
+    }
+    next(err);
+});
+
+// formating errors
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || "Server Error",
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
+    });
+})
 
 
 // exports
